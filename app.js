fb46df9657765a46cb78c683fba049e813c4519b
Annabel Lau <annabel.n.lau@gmail.com>
2017-11-18 16:03:10 -0500

    express sequelize saturday study session

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 const db = require('./db').db;
 const Student = require('./db').Student;
 const Teacher = require('./db').Teacher;
+
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json());
 
 let PORT = 8080;
 
@@ -15,8 +19,120 @@ app.get("/test", (req, res, next) => {
  Your Route Code Here
 */
 
-db.sync({force: true})
-.then(() => {
-	console.log('db synced')
-	app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
-});
+app.get("/students", (req, res, next) => {
+	Student.findAll()
+		.then(allTheStudents => {
+			res.json(allTheStudents)
+		})
+		.catch(next)
+})
+
+app.get("/students/:id", (req, res, next) => {
+	Student.findById(req.params.id)
+		.then(studentID => {
+			res.json(studentID)
+		})
+		.catch(next)
+})
+
+
+app.post("/students/:id", (req, res, next) => {
+	Student.create(req.body)
+	.then(createdStudent => {
+		res.json(createdStudent)
+	})
+	.catch(next)
+})
+
+app.delete("/students/:id", (req, res, next) => {
+	Student.destroy({
+		where: {
+			id: req.params.id 
+		}
+	})
+	.then(function(){
+		res.sendStatus(202)
+	})
+	.catch(next)
+})
+
+app.put("/students/:id/", (req, res, next) => {
+	Student.findOne({
+		where: {
+			id: req.params.id
+		}
+	})
+	.then(foundStudent => {
+		foundStudent.update(req.body)
+	})
+	.then(function(){
+		res.sendStatus(204)
+	})
+	.catch(next)
+})
+
+app.get("/students/:id/getGrade", (req, res, next) => {
+	Student.findOne({
+		where: {
+			id: req.params.id
+		}
+	})
+	.then(retrievedStudent => {
+		getLetterGrade(retrievedStudent.getDataValue("GPA"))
+	})
+	.then(function(){
+		res.send(getLetterGrade(retrievedStudent.getDataValue("GPA")))
+	})
+	.catch(next)
+})
+
+app.get("/getPerfectStudents", (req, res, next) => {
+	Student.getPerfectStudents()
+	.then(gotPerfectStudents => {
+		res.json(gotPerfectStudents)
+	})
+	.catch(next)
+})
+
+app.get("/teachers", (req, res, next) => {
+	Teacher.findAll()
+		.then(allTheTeachers => {
+			res.json(allTheTeachers)
+		})
+		.catch(next)
+})
+
+app.get("/teachers/:id", (req, res, next) => {
+	Teacher.findById(req.params.id)
+		.then(teacherID => {
+			res.json(teacherID)
+		})
+		.catch(next)
+})
+
+app.get("/teachers/:id/students", (req, res, next) => {
+	Student.findAll({
+		where: {
+			teacherId: req.params.id
+		}
+	})
+		.then(foundStudents => {
+			res.json(foundStudents)
+		})
+		.catch(next)
+})
+
+app.post("/teachers/:id", (req, res, next) => {
+	Teacher.create(req.body)
+	.then(createdTeacher => {
+		res.json(createdTeacher)
+	})
+	.catch(next)
+})
+
+
+db.sync(/*{ force: true }*/)
+	.then(() => {
+		console.log('db synced')
+		app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
+	});
--- db.js
+++ db.js
@@ -7,15 +7,51 @@ const db = new Sequelize('postgres://localhost/juniorenrichment', {
 
 
 const Student = db.define("student" , {
-	/* STUDENT MODEL CODE HERE */
-
+	name: {
+		type: Sequelize.STRING
+	},
+	GPA: {
+		type: Sequelize.DECIMAL
+	},
 })
+
+
+Student.getPerfectStudents = function(){
+	return this.findAll({
+		where: {
+			GPA: {
+				$eq: 4.0
+			}
+		}
+	})
+}
+
+Student.prototype.getLetterGrade = function(){
+	const studentsGPA = this.getDataValue("GPA");
+	if(studentsGPA >= 4.0){
+		return "A"
+	} else if (studentsGPA < 4.0 && studentsGPA >= 3.0){
+		return "B"
+	} else if (studentsGPA < 3.0 && studentsGPA >= 2.0){
+		return "C"
+	} else if (studentsGPA < 2.0 && studentsGPA >= 1.0){
+		return "D"
+	} else {
+		return "F"
+	}
+}
 	
 
 const Teacher = db.define('teacher', {
-	/* TEACHER MODEL CODE HERE */
+	name: {
+		type: Sequelize.STRING
+	},
+	subject: {
+		type: Sequelize.STRING
+	}
 
 });
 
+Student.belongsTo(Teacher)
 
 module.exports = {db, Student, Teacher}
